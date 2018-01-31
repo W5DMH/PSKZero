e38fe0b4f46148bfeac27cb6cd3738c926e899ac
W5DMH <34745389+W5DMH@users.noreply.github.com>
2018-01-31 15:24:55 -0500

    Update PBeaconstopped.sh

--- PBeaconstopped.sh
+++ PBeaconstopped.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 
 dialog   --title "Beacon has Stopped!" \
          --textbox "beaconreceived.txt" 8 46 
-
-bash PBeacon.sh
+sudo rm /home/pi/psk31lx/RX/pskreceived.txt
+bash /home/pi/PSKZero/PBeacon.sh
