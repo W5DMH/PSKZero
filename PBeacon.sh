f4002d883119af9b27753c3b74199ff2948eb0a0
W5DMH <34745389+W5DMH@users.noreply.github.com>
2018-01-31 15:21:27 -0500

    Update PBeacon.sh

--- PBeacon.sh
+++ PBeacon.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # A simple shell script to display the form dialog on screen
 # set field names i.e. shell variables
+#sudo modprobe snd-pcm-oss
+#psk31lx &>/dev/null &
 call1=""
 call2=""
 message=""
@@ -8,14 +10,14 @@ message=""
 # open fd
 exec 3>&1
 # Store data to $VALUES variable
-VALUES=$(dialog --extra-button --extra-label "Beacon Off" --ok-label "Beacon On " \
-	  --backtitle "PSKBeacon Setup" \
-	  --title "PSKBeacon" \
-	  --form "Create a new beacon" \
+VALUES=$(dialog --extra-button --extra-label "Beacon Off" --ok-label "Beacon On$
+          --backtitle "PSKBeacon Setup" \
+          --title "PSKBeacon" \
+          --form "Create a new beacon" \
 12 44 0 \
-	"From Callsign:" 1 1	"$call2" 	1 14 8 0 \
-        "To Callsign:"    3 1	"$call1"  	3 14 8 0 \
-	 "Message:"    5 1	"$message"  	5 14 25 0 \
+        "From Callsign:" 1 1    "$call2"        1 14 8 0 \
+        "To Callsign:"    3 1   "$call1"        3 14 8 0 \
+         "Message:"    5 1      "$message"      5 14 26 0 \
 2>&1 1>&3) 
 
 ret=$?
@@ -38,25 +40,21 @@ esac
   read -r message
 } <<< "$VALUES"
 
-beacon="$call1 $call1 de $call2 $call2 $message"
+beacon="$call2 $call2 de $call1 $call1 $message"
 
 if [ "$choice" = 'START' ] 
 then
-   [ -e pskbeacon.txt ] && rm pskbeacon.txt
-   bash ./PBeaconwatch.sh
+   [ -e /home/pi/PSKZero/pskbeacon.txt ] && rm /home/pi/PSKZero/pskbeacon.txt
    # display values just entered
-   echo $beacon >>pskbeacon.txt
+   echo $beacon >>/home/pi/PSKZero/pskbeacon.txt
+   bash /home/pi/PSKZero/PBeaconwatch.sh
 elif [[ "$choice" = 'STOP' ]]; then
   echo 'Beacon has been cancelled'
-  [ -e pskbeacon.txt ] && rm pskbeacon.txt
-  bash ./PBeacon.sh
+  [ -e /home/pi/PSKZero/pskbeacon.txt ] && rm /home/pi/PSKZero/pskbeacon.txt
+  bash PBeacon.sh
 fi
 
 
 
 
-#beacon="$call1 $call1 de $call2 $call2 $message"
-#[ -e pskbeacon.txt ] && rm pskbeacon.txt
-# display values just entered
-#echo $beacon >>pskbeacon.txt
 
